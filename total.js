55d3d2ec8c1b82be18e354f73c4231688f9290bb
Your Name <jahedulislamapurbo@gmail.com>
2025-10-23 17:34:20 +0600

    total-js

---
+++ total.js
@@ -0,0 +1,38 @@
+const numbers = [300, 100, 700, 1200];
+        console.log (numbers)
+// const products = [
+//     {name: 'shampoo', price: 300},
+//     {name: 'chiruni', price: 100},
+//     {name: 'shirt', price: 700},
+//     {name: 'pant', price: 1200}
+// ]
+
+//     function getShoppingTotal (products){
+//         let sum = 0 ;
+//         for (const product of products){
+//             sum = sum + product.price;
+//         }
+//         return sum;
+//     }
+//         const total = getShoppingTotal (products);
+        // console.log ('total spend: ', total);
+
+
+        // shopping cart //
+        const products = [
+    {name: 'shampoo', price: 300, quantity:2},
+    {name: 'chiruni', price: 100, quantity:3},
+    {name: 'shirt', price: 700,quantity:5},
+    {name: 'pant', price: 1200,quantity:1},
+]
+    function cartTotal (products){
+        let total = 0;
+        for (const product of products){
+            console.log (product);
+            const thisProductCost = product.price * product.quantity;
+            total = total + thisProductCost;
+        }
+         return total;
+    }
+        const shoppingCost = cartTotal(products);
+        console.log ('total cost: ',shoppingCost);
